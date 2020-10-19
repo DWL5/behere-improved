6e1daa636e3271db3e6304ca8a88e18338b81df9
kwjjjjj <kwjjjjj@abrain.co.kr>
2020-10-19 17:54:13 +0900

    [added] 조회,수정 화면 추가

---
+++ app/src/main/java/com/behere/location_based_reminder/ui/TodoInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.behere.location_based_reminder.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.behere.location_based_reminder.R
+
+class TodoInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_info)
+    }
+}
